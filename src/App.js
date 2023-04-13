3ec97057e049d85b3d2c8877b91bb44661f05955
worldofchristian <christianwaters@protonmail.com>
2023-04-12 21:41:13 -0600

    fixed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from "react";
-import { useState } from "react";
-import Navbar from "./components/Navbar";
-import NavDropdown from "./components/NavDropdown";
-import Post from "./components/Post";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Navbar toggle={toggle} />
-    <NavDropdown isOpen={!isOpen} toggle={toggle} />
+    <div 
+    className="hero min-h-screen bg-base-200">
+      <div 
+      className="hero-content text-center">
+        
+        <div 
+        className="max-w-md">
+          <h1 
+          className="text-5xl font-bold"
+          >A comfortable place to learn about the brain</h1>
 
-    <div
-    className="flex items-center justify-center">
-      <Post />
+          <Link to='/explore'>
+            <button 
+            className="btn btn-primary mt-6"
+            >Explore topics</button>
+          </Link>
+        </div>
+      
+      </div>
     </div>
-
     </>
   );
 }
