fb33de9ca1be5d1781c09e509e5ac3614acb1fac
worldofchristian <christianwaters@protonmail.com>
2023-10-02 21:35:28 -0600

    dorsal/ventral

--- src/components/VisualSystem.js
+++ src/components/VisualSystem.js
@@ -5,22 +5,22 @@ import 'reactflow/dist/style.css';
 const initialNodes = [
   {
     id: 'retina',
-    position: { x: 75, y: 100 },
+    position: { x: 150, y: 100 },
     data: { label: 'Retina: Captures light and converts it into electrical signals.' },
   },
   {
     id: 'lgn',
-    position: { x: 200, y: 300 },
+    position: { x: 275, y: 300 },
     data: { label: 'LGN (Lateral Geniculate Nucleus): Acts as a relay station, sending signals to V1.' },
   },
   {
     id: 'v1',
-    position: { x: 200, y: 450 },
+    position: { x: 275, y: 450 },
     data: { label: 'V1: Responsible for edge detection, basic shapes, and some color processing.' },
   },
   {
     id: 'v2',
-    position: { x: 200, y: 600 },
+    position: { x: 275, y: 600 },
     data: { label: 'V2: Constructs more complex forms like intricate shapes and object recognition.' },
   },
   {
@@ -30,62 +30,62 @@ const initialNodes = [
   },
   {
     id: 'mt',
-    position: { x: 400, y: 1100 },
+    position: { x: 400, y: 1200 },
     data: { label: 'MT (Middle Temporal area): Processing motion, perceiving the speed and direction of moving objects.' },
   },
   {
     id: 'mst',
-    position: { x: 500, y: 1300 },
+    position: { x: 400, y: 1400 },
     data: { label: 'MST (Medial Superior Temporal area): Analyzing more complex aspects of motion, including the motion of objects relative to the background.' },
   },
   {
     id: 'ppc', 
-    position: { x: 500, y: 1550 },
+    position: { x: 400, y: 1650 },
     data: { label: 'Posterior Parietal Cortex: Involved in spatial awareness and sensory integration.' },  
   },
   {
     id: 'fef',
-    position: { x: 500, y: 1750 },
+    position: { x: 400, y: 1800 },
     data: { label: 'Frontal Eye Fields (FEF): Controls voluntary eye movements and visual attention.' },
   },
   {
     id: 'spl',
-    position: { x: 400, y: 2000 },
+    position: { x: 300, y: 2000 },
     data: { label: 'Superior Parietal Lobule (SPL): Involved in spatial relations and coordination.' },
   },
   {
     id: 'precuneus',
-    position: { x: 400, y: 2200 },
+    position: { x: 300, y: 2200 },
     data: { label: 'Precuneus: Plays a role in episodic memory, visuospatial processing, and self-reflection.' },
   },
   {
     id: 'ventral',
-    position: { x: 75, y: 825 },
+    position: { x: 150, y: 825 },
     data: { label: 'Ventral Stream: Responsible for object recognition and identification.' },
   },
   {
     id: 'v4',
-    position: { x: 75, y: 950 },
+    position: { x: 150, y: 1000 },
     data: { label: 'V4: Processes color and shape, helps in object recognition.' },
   },
   {
     id: 'it',
-    position: { x: 75, y: 1200 },
+    position: { x: 150, y: 1400 },
     data: { label: 'IT (Inferotemporal Cortex): Involved in recognition and identification of objects.' },
   },
   {
     id: 'perirhinal',
-    position: { x: -50, y: 1500 },
+    position: { x: 100, y: 1800 },
     data: { label: 'Perirhinal Cortex: Involved in object recognition and memory.' },
   },
   {
     id: 'ffa',
-    position: { x: -50, y: 1700 },
+    position: { x: 100, y: 2000 },
     data: { label: 'FFA (Fusiform Face Area): Specialized for facial recognition.' },
   },
   {
     id: 'ppa',
-    position: { x: 75, y: 2000 },
+    position: { x: 100, y: 2200 },
     data: { label: 'PPA (Parahippocampal Place Area): Involved in recognizing places and scenes.' },
   }
 ];
